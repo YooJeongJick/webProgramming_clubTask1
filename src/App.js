fd24da9df23be492a11464d25b04d8b79b1285f7
Yoo Jeong <04dbwjd@gmail.com>
2024-03-12 09:13:22 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <textarea readOnly
+        rows="10" cols="75" id="textOutput"></textarea>
       </header>
+      <footer>
+        <input type="text" size="75" id="textInput"></input>
+      </footer>
     </div>
   );
 }
