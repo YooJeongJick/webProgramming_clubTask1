f43c61c10c771aa97c93170497ee80022b6c5868
Yoo Jeong <04dbwjd@gmail.com>
2024-03-13 22:10:17 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,45 @@
 import './App.css';
+import { useState } from "react";
 
 function App() {
+
+  const [names, setNames] = useState([
+    {id : 0, text : ''}
+  ]);
+  const [nextId, setNextId] = useState(1);
+  const [nextText, setNextText] = useState('');
+
+  const onchangeInput = e => {
+    setNextText(e.target.value);
+  }
+
+  const onkeydownInput = e => {
+    if (e.key === 'Enter') {
+      const nextNames = names.concat({
+        id : nextId,
+        text : nextText,
+      });
+      setNames(nextNames);
+      setNextId(nextId + 1);
+      setNextText('');
+    }
+  }
+
   return (
     <div>
-      <header>
-        <textarea readOnly
-        rows="10" cols="75" id="textOutput"></textarea>
+      <header id="textOutput">
+        {names.map(name => (
+          <div key={name.id}>
+            {name.text}
+          </div>
+        ))}
       </header>
       <footer>
-        <input type="text" size="75" id="textInput"></input>
+        <input type="text" size="70" id="textInput"
+        value={nextText}
+        onChange={onchangeInput}
+        onKeyDown={onkeydownInput}>
+        </input>
       </footer>
     </div>
   );
