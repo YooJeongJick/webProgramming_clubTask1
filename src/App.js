5b1f113462be2e5e568836234edab3192f9b2f56
Yoo Jeong <04dbwjd@gmail.com>
2024-03-14 09:46:53 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -1,46 +1,68 @@
-import './App.css';
-import { useState } from "react";
+import styled from 'styled-components';
+import { useState } from 'react';
+
+const TextOutput = styled.header`
+  width: 500px;
+  height: 300px;
+
+  border-width: 1px;
+  border-color: #000;
+  border-style: dashed;
+
+  overflow-y: auto;
+  word-wrap: break-word;
+
+  display: block;
+  margin: 0 auto;
+  margin-top: 10px;
+  padding: 10px;
+`;
+
+const TextInput = styled.footer`
+  display: flex;
+  justify-content: center;
+  margin: 10px;
+`;
 
 function App() {
-
-  const [names, setNames] = useState([
-    {id : 0, text : ''}
-  ]);
+  const [names, setNames] = useState([{ id: 0, text: '' }]);
   const [nextId, setNextId] = useState(1);
   const [nextText, setNextText] = useState('');
 
-  const onchangeInput = e => {
+  const onchangeHandle = (e) => {
     setNextText(e.target.value);
-  }
+  };
 
-  const onkeydownInput = e => {
+  const onkeydownHandle = (e) => {
     if (e.key === 'Enter') {
       const nextNames = names.concat({
-        id : nextId,
-        text : nextText,
+        id: nextId,
+        text: nextText,
       });
       setNames(nextNames);
       setNextId(nextId + 1);
       setNextText('');
     }
-  }
+  };
 
   return (
     <div>
-      <header id="textOutput">
-        {names.map(name => (
-          <div key={name.id}>
-            {name.text}
-          </div>
+
+      <TextOutput>
+        {names.map((name) => (
+          <div key={name.id}>{name.text}</div>
         ))}
-      </header>
-      <footer>
-        <input type="text" size="70" id="textInput"
-        value={nextText}
-        onChange={onchangeInput}
-        onKeyDown={onkeydownInput}>
-        </input>
-      </footer>
+      </TextOutput>
+
+      <TextInput>
+        <input
+          type="text"
+          size="70"
+          value={nextText}
+          onChange={onchangeHandle}
+          onKeyDown={onkeydownHandle}/>
+      </TextInput>
+
     </div>
   );
 }
